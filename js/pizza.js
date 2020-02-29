04d84a9b9e3af1ec4ad685907672812de3f902af
barackmaund1 <pmaundu49@gmail.co>
2020-02-29 11:40:39 +0300

    add form

---
+++ js/pizza.js
@@ -0,0 +1,12 @@
+// business logic
+// constructor function
+function selectedPizza(pname,psize,pcrust,ptopping,total) {
+    this.pname=name;
+    this.psize=size;
+    this.pcrust=crust;
+    this.ptopping=[];
+    this.total=total;
+}
+selectedPizza.prototype.total=function() {
+    return this. price + crustPrice + toppingprice 
+}
